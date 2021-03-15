7a582a5d60664fcf07e8e9dfc3bfa0113349bd21
Nagy Krisztina Dalma <dnagykrisztina@sch.bme.hu>
2021-03-15 15:43:27 +0100

    Add database content

---
+++ Tables/StaticData/Person.PhoneNumberType.Data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT Person.PhoneNumberType ON
+GO
+INSERT Person.PhoneNumberType(PhoneNumberTypeID, Name, ModifiedDate) VALUES (1, N'Cell', '2017-12-13 13:19:22.273')
+INSERT Person.PhoneNumberType(PhoneNumberTypeID, Name, ModifiedDate) VALUES (2, N'Home', '2017-12-13 13:19:22.273')
+INSERT Person.PhoneNumberType(PhoneNumberTypeID, Name, ModifiedDate) VALUES (3, N'Work', '2017-12-13 13:19:22.273')
+GO
+SET IDENTITY_INSERT Person.PhoneNumberType OFF
+GO
---
+++ Tables/StaticData/Sales.ShoppingCartItem.Data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT Sales.ShoppingCartItem ON
+GO
+INSERT Sales.ShoppingCartItem(ShoppingCartItemID, ShoppingCartID, Quantity, ProductID, DateCreated, ModifiedDate) VALUES (2, N'14951', 3, 862, '2013-11-09 17:54:07.603', '2013-11-09 17:54:07.603')
+INSERT Sales.ShoppingCartItem(ShoppingCartItemID, ShoppingCartID, Quantity, ProductID, DateCreated, ModifiedDate) VALUES (4, N'20621', 4, 881, '2013-11-09 17:54:07.603', '2013-11-09 17:54:07.603')
+INSERT Sales.ShoppingCartItem(ShoppingCartItemID, ShoppingCartID, Quantity, ProductID, DateCreated, ModifiedDate) VALUES (5, N'20621', 7, 874, '2013-11-09 17:54:07.603', '2013-11-09 17:54:07.603')
+GO
+SET IDENTITY_INSERT Sales.ShoppingCartItem OFF
+GO
