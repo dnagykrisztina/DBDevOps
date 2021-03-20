f0c2f4858704e1c7a3f427d62bb5cbe7bbbeeb82
Nagy Krisztina Dalma <dnagykrisztina@sch.bme.hu>
2021-03-20 15:45:03 +0100

    Update unit test

--- Programmability/Procedures/Test.test MyUnitTest_1.sql
+++ Programmability/Procedures/Test.test MyUnitTest_1.sql
@@ -6,21 +6,7 @@ GO
 CREATE PROCEDURE [Test].[test MyUnitTest_1]
 AS
 BEGIN
-  --Assemble
-  --  This section is for code that sets up the environment. It often
-  --  contains calls to methods such as tSQLt.FakeTable and tSQLt.SpyProcedure
-  --  along with INSERTs of relevant data.
-  --  For more information, see http://tsqlt.org/user-guide/isolating-dependencies/
-
-  --Act
-  --  Execute the code under test like a stored procedure, function or view
-  --  and capture the results in variables or tables.
-
-  --Assert
-  --  Compare the expected and actual values, or call tSQLt.Fail in an IF statement.
-  --  Available Asserts: tSQLt.AssertEquals, tSQLt.AssertEqualsString, tSQLt.AssertEqualsTable
-  --  For a complete list, see: http://tsqlt.org/user-guide/assertions/
-  EXEC tSQLt.Fail 'TODO:Implement this test.'
+  EXEC tSQLt.AssertObjectExists @ObjectName = N'[Person].[Pet]', @Message = N'Fail'
 
 END;
 GO
