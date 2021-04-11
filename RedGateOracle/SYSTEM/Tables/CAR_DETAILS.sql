8441ceadaada38a5dea2f75ef6dcec567c968ba6
dnagykrisztina <dnagykrisztina@>
2021-04-11 12:37:53 +0200

    Make changes on CAR_DETAILS

--- RedGateOracle/SYSTEM/Tables/CAR_DETAILS.sql
+++ RedGateOracle/SYSTEM/Tables/CAR_DETAILS.sql
@@ -3,10 +3,11 @@ CREATE TABLE "SYSTEM".car_details (
   mpg NUMBER(5,2),
   cylinders NUMBER(5),
   edispl NUMBER(10),
-  horsepower NUMBER(10),
   weight NUMBER(10),
   accel NUMBER(10,2),
   "YEAR" NUMBER(10),
+  horsepower2 NUMBER(*,0),
+  color VARCHAR2(25 BYTE),
   CONSTRAINT car_details_id_pk PRIMARY KEY ("ID"),
   CONSTRAINT car_details_id_fk FOREIGN KEY ("ID") REFERENCES "SYSTEM".car_names ("ID")
 );
