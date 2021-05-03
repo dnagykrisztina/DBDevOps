3f786765ba7b34ad8afcf7c056333c62eb6ef399
Nagy Krisztina Dalma <dnagykrisztina@sch.bme.hu>
2021-05-03 21:58:39 +0200

    Save static data

---
+++ Data/dbo.AWBuildVersion_Data.sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[AWBuildVersion] ON
+INSERT INTO [dbo].[AWBuildVersion] ([SystemInformationID], [Database Version], [VersionDate], [ModifiedDate]) VALUES (1, N'13.0.1601.5', '2016-04-29 23:23:58.000', '2017-10-19 00:00:00.000')
+SET IDENTITY_INSERT [dbo].[AWBuildVersion] OFF
